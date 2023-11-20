a3e0da1207d95da36d705cd40677843e053d6946
Andres Masis <andres.masis.rojas@gmail.com>
2023-11-20 15:18:55 +0100

    cube Commit

--- PythonPrototype/main.py
+++ PythonPrototype/main.py
@@ -5,6 +5,6 @@ location = (1, 1, 1)
 color = (0, 0, 0)
 
 manager = euclidViewerCoppelia.EuclidViewerCoppelia()
-x = manager.addCircle(location, [], 1, color, "Summertime", False, False)
+x = manager.addCube(location, [], 0.5, color, "Tanzkurs", True)
 time.sleep(8)
 manager.stopSimulation()
